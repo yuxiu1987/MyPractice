c5248431ab1970bb0718e80b29ca238fbc67815b
yuxiu1987 <yuxiu1987@live.cn>
2017-03-28 22:52:20 +0800

    20170328

--- DragAndDropSample/DragAndDropSample/MainWindow.xaml.cs
+++ DragAndDropSample/DragAndDropSample/MainWindow.xaml.cs
@@ -23,7 +23,14 @@ namespace DragAndDropSample
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            liuweilabel.DataContext = liuwei;
+        }
+        Student liuwei = new Student() { name = "Liu Wei", age = 17, sex = "Male" };
 
         #region label的content属性拖拽进listbox
         private void label_MouseMove(object sender, MouseEventArgs e)
@@ -104,8 +111,14 @@ namespace DragAndDropSample
             if ((_rec != null) && e.LeftButton == MouseButtonState.Pressed)
             {
 
-                ((Grid)_rec.Parent).Children.Remove(_rec);
-                Rectangle datacontent = _rec;
+
+                Rectangle datacontent = new Rectangle
+                {
+                    Height = _rec.Height,
+                    Width = _rec.Width,
+                    Fill = _rec.Fill,
+                    StrokeThickness = 0.25
+                };
 
                 DataObject dataobj = new DataObject("myrec", datacontent);
                 DragDrop.DoDragDrop(rec, dataobj, DragDropEffects.Copy);
@@ -145,13 +158,67 @@ namespace DragAndDropSample
             var gd = sender as Grid;
             if (e.Data.GetDataPresent("myrec"))
             {
-                var temp = e.Data.GetData("myrec");
+                var temp = e.Data.GetData("myrec");                
+                gd.Children.Remove(temp as Rectangle);
                 gd.Children.Add(temp as Rectangle);
             }
-        }        
+        }
+
         #endregion
 
+        private void Label_MouseMove_2(object sender, MouseEventArgs e)
+        {
+            var _label = sender as Label;
+            if((_label!=null)&& e.MouseDevice.LeftButton == MouseButtonState.Pressed)
+            {
+                var datacontent = _label.DataContext;
+                DataObject dataobj = new DataObject("mydata", datacontent);
+                DragDrop.DoDragDrop(_label, dataobj, DragDropEffects.Copy);
+            }
+        }
 
+        private void StackPanel_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent("mydata"))
+            {
+                var data = e.Data.GetData("mydata") as Student;
+                var container = sender as StackPanel;
+                container.DataContext = data;
+            }
+        }
+
+        private void StackPanel_DragLeave(object sender, DragEventArgs e)
+        {
+            var container = sender as StackPanel;
+            container.DataContext = null;
+        }
+
+        private void StackPanel_DragOver(object sender, DragEventArgs e)
+        {
+            if(!e.Data.GetDataPresent("mydata"))
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void StackPanel_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("mydata"))
+            {
+                var data = e.Data.GetData("mydata") as Student;
+                var container = sender as StackPanel;
+                container.DataContext = data;
+            }
+        }
+
+
+    }
+
+    public class Student
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string sex { get; set; }
     }
     
 
