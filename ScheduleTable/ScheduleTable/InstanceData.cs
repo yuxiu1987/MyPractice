0384dd8d3e5f3a3a9c1a1cfb5cdf2cb35138d945
yuxiu1987 <yuxiu1987@live.cn>
2017-03-29 19:45:01 +0800

    20170329

---
+++ ScheduleTable/ScheduleTable/InstanceData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleTable
+{
+
+    public class SchedulePerWeek
+    {
+        #region 单例
+        private SchedulePerWeek()
+        {
+
+        }
+
+        private static SchedulePerWeek _SchedulePerWeek;
+
+        public static SchedulePerWeek Instance
+        {
+            get
+            {
+                if (_SchedulePerWeek == null) { _SchedulePerWeek = new SchedulePerWeek(); }
+                return _SchedulePerWeek;
+            }
+            set { }
+        }
+        #endregion
+
+        public List<Flight> FlightsPerWeek { get; set; } = new List<Flight>();
+    }
+}
---
+++ ScheduleTable/ScheduleTable/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleTable
+{
+    public class Flight
+    {
+        public string FlightNumber { get; set; }
+        public DateTime starttime { get; set; }
+        public DateTime endtime { get; set; }
+        public TimeSpan timelength { get; set; }
+        public DayOfWeek StartDay { get; set; }
+        public DayOfWeek EndDay { get; set; }
+    }
+}
+
