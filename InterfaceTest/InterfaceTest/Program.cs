577e3637b206c6b10bbb0ca903758c386c44e63f
yuxiu1987 <yuxiu1987@live.cn>
2017-04-22 02:09:33 +0800

    422-2

---
+++ InterfaceTest/InterfaceTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PassengerAirliner B707 = new PassengerAirliner();
+            CargoAirliner DC10F = new CargoAirliner();
+            DC10F.Carry();
+            B707.Carry();
+
+            Console.ReadKey();
+
+        }
+    }
+
+    
+    public interface ICarry
+    {
+       void Carry();
+    }
+
+    public class PassengerAirliner : ICarry
+    {
+        public void Carry()
+        {
+            Console.WriteLine("300Passengers");
+        }        
+    }
+
+    public class CargoAirliner
+    {
+        public void Carry()
+        {
+            Console.WriteLine("100TonsCargoes");
+        }
+    }
+}
